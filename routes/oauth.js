fb48b37ed459c7d13d6c2bc53620300d810521ca
Victor Borja <vborja@apache.org>
2015-04-26 18:34:00 -0500

    He who hoots with owls by night cannot soar with eagles by day.

--- routes/oauth.js
+++ routes/oauth.js
@@ -16,9 +16,9 @@ router.get('/', function (req, res) {
 })
 
 router.get('/callback', function (req, res) {
-  sys.puts(">>"+req.session.oauthRequestToken);
-  sys.puts(">>"+req.session.oauthRequestTokenSecret);
-  sys.puts(">>"+req.query.oauth_verifier);
+  console.log(">>"+req.session.oauthRequestToken);
+  console.log(">>"+req.session.oauthRequestTokenSecret);
+  console.log(">>"+req.query.oauth_verifier);
   var consumer = twitterOauth();
   consumer.getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
     if (error) {
@@ -26,13 +26,14 @@ router.get('/callback', function (req, res) {
     } else {
       req.session.oauthAccessToken = oauthAccessToken;
       req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
+      console.log("AUTH ACCES ", oauthAccessToken, oauthAccessTokenSecret);
       // Right here is where we would write out some nice user stuff
       consumer.get("http://twitter.com/account/verify_credentials.json", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
         if (error) {
           res.send("Error getting twitter screen name : " + sys.inspect(error), 500);
         } else {
           req.session.twitterScreenName = data["screen_name"];    
-          sys.puts(sys.inspect(req.session));
+          console.log(sys.inspect(req.session));
           res.send('You are signed in: ' + req.session.twitterScreenName)
         }  
       });  
