b51a312f2a53964105d4e90071d6e5f4265ef4e4
Victor Borja <vborja@apache.org>
2015-04-26 18:29:06 -0500

    session

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({secret: process.env.session_key}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
