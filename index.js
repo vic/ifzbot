dcdcfc01750c812d5dc3b6f57cd3aff5c5eb4bb1
Victor Borja <vborja@apache.org>
2015-04-26 16:29:51 -0500

    ittt

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var webhook = require('express-ifttt-webhook');
+app.use(webhook());
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(process.env['PORT'] || 3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
