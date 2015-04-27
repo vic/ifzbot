7e7d2968e6b6f3c8e80dfa0c9ff07fc4e433cc0e
Victor Borja <vborja@apache.org>
2015-04-26 23:08:57 -0500

    Add libs

--- routes/ifz.js
+++ routes/ifz.js
@@ -1,22 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Twit = require('twit');
-var sys = require('sys');
 
-var T = new Twit({
-    consumer_key:         process.env.consumer_key
-  , consumer_secret:      process.env.consumer_secret
-  , access_token:         process.env.ifzbot_key
-  , access_token_secret:  process.env.ifzbot_secret
-})
-
-function reply(tweet_url, screen_name, status, cb) {
-  var id = tweet_url.match(/[\w-]+$/)[0];
-  return T.post('statuses/update', {
-    status: sys.format('@%s %s', screen_name, status),
-    in_reply_to_status_id: id
-  }, cb);
-}
 
 router.post('/', function(req, res, next) {
   console.log(">>>>>>>> ", sys.inspect(req.body));
