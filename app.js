37a4dee783e824518a9325485da0cbb02be6b264
Victor Borja <vborja@apache.org>
2015-04-26 19:05:18 -0500

    echo

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
 app.use('/oauth', require('./routes/oauth'));
+app.use('/ifz', require('./routes/ifz'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
